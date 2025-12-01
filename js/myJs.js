ce1cfb946e396d7ef23228347f47e51534cb3962
Melwx <thanh50687@gmail.com>
2025-12-01 12:07:29 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -1,16 +1,16 @@
 const textConfig = {
-  text1: "He luu cậu!",
-  text2: "Tớ có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa:3333",
-  text3: "Cậu yêu tớ có phải không nào ._.",
-  text4: "Nếu cậu ko trả lời mà thoát ra tức là muốn làm vợ tớ rùi đó nha :v",
-  text5: "Cậu mơ à???",
+  text1: "He luu emm!",
+  text2: "Anh có điều này muốn hỏi cậu nhớ phải trả lời thật lòng nhaaa:3333",
+  text3: "Em yêu anh có phải không nào ._.",
+  text4: "Nếu em ko trả lời mà thoát ra tức là muốn làm vợ anh rùi đó nha :v",
+  text5: "Anh mơ à???",
   text6: "Yêu ơi là yêu <3",
-  text7: "lí do cậu thích tớ đi :vvvv",
-  text8: "Gửi cho tớ <3",
-  text9: "Vì cậu đẹp try và dthuong vlll:>>",
-  text10: "Tớ biết mà ^^ Yêu cậux 1000.000,
+  text7: "lí do em thích anh đi :vvvv",
+  text8: "Gửi cho anh <3",
+  text9: "Vì anh đẹp trai và học giỏi vl:>>",
+  text10: "anh biết mà ^^ Yêu em",
   text11:
-    "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
+    "Thế từ giờ mình là người yêu nháaa",
   text12: "Okii lunn <3",
 };
 
@@ -153,4 +153,5 @@ $(document).ready(function () {
       });
     });
   });
+
 });
