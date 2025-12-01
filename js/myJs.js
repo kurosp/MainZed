960eb64123d6557b74f5165891e65356f063bff5
Melwx <thanh50687@gmail.com>
2025-12-01 12:30:23 +0700

    Update myJs.js

--- js/myJs.js
+++ js/myJs.js
@@ -137,7 +137,7 @@ $(document).ready(function () {
           text: textConfig.text11,
           confirmButtonColor: "#83d0c9",
           onClose: () => {
-            window.location = "https://fb.com/nguyenphan299";
+            window.location = "https://fb.com/ToanNamChinh";
           },
         });
       }
@@ -153,3 +153,4 @@ $(document).ready(function () {
     });
   });
 });
+
